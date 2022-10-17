8539e8b71a46abd7bfa4cb8486029bd3057f00d7
ys03055 <ys03055@naver.com>
2022-10-17 22:41:27 +0900

    회전 후 모래시계합

--- 모래시계 합.py
+++ 모래시계 합.py
@@ -11,3 +11,28 @@ import sys
 # 만약 회전명령 정보가 2 0 3이면 2번째 행을 왼쪽으로 3만큼 아래 그림처럼 회전시키는 명령 입니다.
 
 
+n= int(input())
+a= [list(map(int, input().split())) for _ in range(n)]
+m = int(input())
+for i in range(m):
+    h,t,k = map(int, input().split())
+    if t==0:
+        for _ in range(k):
+            a[h-1].append(a[h-1].pop(0))   #h행 1번째열 값을 마지막으로 옮김
+    else :
+        for _ in range(k):
+            a[h-1].insert(0,a[h-1].pop(0))   #h행 1번째열 값에 대입
+res = 0
+s= 0
+e = n-1
+for i in range(n):
+    for j in range(s, e+1):
+        res+=a[i][j]
+    if i < n//2:
+        s+=1
+        e-=1
+    else :
+        s-=1
+        e+=1
+print(res)            
+
