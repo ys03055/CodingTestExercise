58c02176d6c562bba3e167658cb5bd6cdf6931cc
ys03055 <ys03055@naver.com>
2022-10-18 23:52:00 +0900

    최소용량 구하기 문제

---
+++ 최소용량.py
@@ -0,0 +1,32 @@
+import sys
+
+# sys.stdin= open("input.txt", "rt")
+
+
+#최소 용량 크기 구하기
+
+def Count(capacity):
+    cnt=1
+    sum=0
+    for x in Music:
+        if sum+x>capacity:
+            cnt+=1
+            sum=x
+        else:
+            sum+=x
+    return cnt
+
+n, m=map(int, input().split())
+Music=list(map(int, input().split()))
+maxx=max(Music)
+lt=1
+rt=sum(Music)
+res=0
+while lt<=rt:
+    mid=(lt+rt)//2
+    if mid>=maxx and Count(mid)<=m:
+        res=mid
+        rt=mid-1
+    else:
+        lt=mid+1
+print(res)
