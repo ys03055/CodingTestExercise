e7e6743ce64820cb4064585e2c55899fa3a9619e
ys03055 <ys03055@naver.com>
2022-10-19 23:31:38 +0900

    마굿간 배정

---
+++ 마굿간 배정.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin=open("input.txt", "r")
+
+
+n, c=map(int, input().split())
+Line=[]
+for _ in range(n):
+    tmp=int(input())
+    Line.append(tmp)
+Line.sort()
+lt=1
+rt=Line[n-1]
+while lt<=rt:
+    mid=(lt+rt)//2
+    if Count(mid)>=c:
+        res=mid
+        lt=mid+1
+    else:
+        rt=mid-1
+
+print(res)
