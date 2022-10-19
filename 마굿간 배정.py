684d6d31762c22b0c237f7ecc1ff06e5bf2f2642
ys03055 <ys03055@naver.com>
2022-10-19 23:33:57 +0900

    마굿간 배장_해석추가

--- 마굿간 배정.py
+++ 마굿간 배정.py
@@ -1,6 +1,6 @@
 import sys
 sys.stdin=open("input.txt", "r")
-def Count(len):
+def Count(len):     #배열 차이 구하는 함수
     cnt=1
     ep=Line[0]
     for i in range(1, n):
@@ -14,11 +14,11 @@ Line=[]
 for _ in range(n):
     tmp=int(input())
     Line.append(tmp)
-Line.sort()
+Line.sort()     #문제에 따라 오름차순 정렬.
 lt=1
 rt=Line[n-1]
 while lt<=rt:
-    mid=(lt+rt)//2
+    mid=(lt+rt)//2    #이분 검색 사용
     if Count(mid)>=c:
         res=mid
         lt=mid+1
