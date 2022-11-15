a79af232dfb92f3f4e20d24952733418bc252afe
ys03055 <ys03055@naver.com>
2022-11-15 23:59:11 +0900

    프로그래머스_피로도 추가

--- 피로도.py
+++ 피로도.py
@@ -1,12 +1,13 @@
 def solution(k, dungeons):
-    cnt = 0
     answer = -1
-    a = len(dungeons)
-    for i in range(len(dungeons)) :
-        if k >=dungeons[i][0] :
-            k -=dungeons[i][1]
-            cnt +=1
-        else :
-            continue
-    print(cnt)
+    for i in range(len(dungeons)):
+        cnt = 0
+        for j in range(len(dungeons)) :
+            if k >=dungeons[i][0] :
+                k -=dungeons[i][1]
+                cnt +=1
+            else :
+                continue
+            print(k)
+            print(cnt)
     return answer
